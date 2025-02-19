a53601ac603fd2b4be47ed1c0f685fa4226ec9c4
Hemel <rh189827@gmail.com>
2025-02-20 00:37:40 +0600

    Event listener for Add button click

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const todoForm = document.querySelector('form') 
 const todoInput = document.querySelector('#todoInput'); 
 const todoList = document.querySelector('#todoList');
+let allTodos = [];
+
+//Event listener for Add button click
+todoForm.addEventListener('submit', function (e){
+    e.preventDefault();
+})
