5cda0371a23e73b9d736ffa3452e66410710c582
Hemel <rh189827@gmail.com>
2025-02-20 00:43:27 +0600

    Add ToDo Function to fill the array

--- index.js
+++ index.js
@@ -6,4 +6,15 @@ let allTodos = [];
 //Event listener for Add button click
 todoForm.addEventListener('submit', function (e){
     e.preventDefault();
+    addTodo();
 })
+
+//Add ToDo Function to fill the array
+function addTodo() {
+    const todoText = todoInput.value.trim();
+    if (todoText.length > 0){
+        allTodos.push(todoText);
+        todoInput.value = '';
+    }
+    
+}
