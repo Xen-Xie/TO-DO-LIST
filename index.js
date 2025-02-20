7e1c2633eba023ca487be02d3fa15e50db54086e
Hemel <rh189827@gmail.com>
2025-02-20 12:17:39 +0600

    Delete button Function added with key changes

--- index.js
+++ index.js
@@ -14,7 +14,11 @@ todoForm.addEventListener('submit', function (e){
 function addTodo() {
     const todoText = todoInput.value.trim();
     if (todoText.length > 0){
-        allTodos.push(todoText);
+        const todoObject = {
+            text: todoText,
+            completed: false
+        }
+        allTodos.push(todoObject);
         updateTodoList();
         saveTodo();
         todoInput.value = '';
@@ -33,11 +37,12 @@ function updateTodoList() {
 //Main Function to create a new TodoList
 function createTodoItem(todo, todoIndex) {
     const todoLi = document.createElement('li');
+    const todoText = todo.text;
     const todoId = 'todo-'+todoIndex;
     todoLi.className = "todo";
 
     todoLi.innerHTML = ` 
-      <li id="${todoId}" class="mb-[10px] px-6 py-0 bg-[#1c1d20] rounded-[15px] flex items-center">
+      <li id="${todoId}" class="mb-[10px] px-6 py-0 bg-[#1c1d20] rounded-[15px] flex items-center text-white font-mono">
         <input type="checkbox" id="checkbox-${todoId}" class="peer hidden"> <!-- Changed ID to avoid conflict -->
         
         <label for="checkbox-${todoId}" class="border-2 border-[#00ffc4] rounded-full min-w-[20px] min-h-[20px] flex justify-center items-center flex-shrink-0 transition-all duration-200 peer-checked:bg-[#00ffc4]">
@@ -45,8 +50,8 @@ function createTodoItem(todo, todoIndex) {
                 <path d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/>
             </svg>
         </label>
-        <label for="checkbox-${todoId}" class="p-15px pr-0 flex-grow peer-checked:line-through peer-checked:text-[#4A4D57] transition-all duration-200">
-            ${todo}
+        <label for="checkbox-${todoId}" class="p-15px pr-0 flex-grow peer-checked:line-through peer-checked:text-[#4A4D57] transition-all duration-200 ml-3">
+            ${todoText}
         </label>
         <button id="deleteButton" class="p-[3px] bg-none border-none flex justify-center cursor-pointer">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="h-[24px] fill-[#4A4D57] hover:fill-[#ff0033] transition-all duration-300">
@@ -55,7 +60,23 @@ function createTodoItem(todo, todoIndex) {
         </button>
     </li>
 `
+ //todo Delete Button
+    const deleteButton = todoLi.querySelector('#deleteButton');
+    deleteButton.addEventListener('click', function (){
+        deleteTodoItem(todoIndex);
+    });
+    const checkbox = todoLi.querySelector('input');
+    checkbox.addEventListener('change', ()=>{
+        allTodos[todoIndex].completed = checkbox.checked;
+        saveTodo();
+    });
+    checkbox.checked = todo.completed;
     return todoLi;
+}
+function deleteTodoItem(todoIndex){
+    allTodos = allTodos.filter((_,i)=> i !== todoIndex);
+    saveTodo();
+    updateTodoList();
 }
 //Save Todo list in local Storage
 function saveTodo(){
